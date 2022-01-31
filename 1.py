fa6b6ac73264febaaa87eb0ea34a5877cbe0ed42
MariosKonstantinosMarouditsisRapanakis <marios.k.r.marouditsis@gmail.com>
2022-01-31 17:31:52 +0200

    Added the forgotten rule

--- 1.py
+++ 1.py
@@ -34,6 +34,22 @@ def checkEnd(ring, row, column):
         #diagonal 2
         if((ring in board[0][2]) and (ring in board[2][0])):
             return True
+    #bigger to smaller
+    if('medium' in board[row][1]):
+        if((('small' in board[row][0]) and ('big' in board[row][2])) or (('big' in board[row][0]) and ('small' in board[row][2]))):
+            return True
+    if('medium' in board[1][column]):
+        if((('small' in board[0][column]) and ('big' in board[2][column])) or (('big' in board[0][column]) and ('small' in board[2][column]))):
+            return True
+    #diagonals
+    if('medium' in board[1][1]):
+        #diagonal 1
+        if((('big' in board[0][0]) and ('small' in board[2][2])) or (('small' in board[0][0]) and ('big' in board[2][2]))):
+            return True
+        #diagonal 2
+        if((('small' in board[0][2]) and ('big' in board[2][0])) or (('big' in board[0][2]) and ('small' in board[2][0]))):
+            return True
+    return False
 
 sum = 0
 for i in range(10000):
